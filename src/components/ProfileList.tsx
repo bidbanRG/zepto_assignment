5df64fb0707c333d867d40fedbaf0eb5b806edf4
@bidesh2 <bidesh@monktechlabs.com>
2024-01-16 17:36:58 +0530

    loading indicator added on profile search

--- src/components/ProfileList.tsx
+++ src/components/ProfileList.tsx
@@ -10,7 +10,12 @@ type Props = {
 export default function ProfileList({ profiles, onSelectProfile }: Props) {
 
    return <div className="absolute bg-white w-[400px] shadow-2xl ml-2 h-[300px] overflow-y-scroll search-results">
-      {profiles.map((props) => <Profile key={props.node_id} {...props} onSelectProfile={onSelectProfile} />)}
+      {profiles.map((props) => 
+         <Profile 
+            key={props.node_id}
+            {...props}
+            onSelectProfile={onSelectProfile}
+         />)}
    </div>
 
 }
